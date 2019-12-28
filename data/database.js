c3c97e2068124795ee9f202a02175b2428934325
Slava Beliaev <dn301182bva@gmail.com>
2019-12-29 00:40:24 +0200

    Update database.js
    swithc to prod

--- data/database.js
+++ data/database.js
@@ -1,26 +1,26 @@
 const Sequelize = require('sequelize');
 
-// Prod
-// const sequelize = new Sequelize(
-//     '5IU45193DS',
-//     '5IU45193DS',
-//     'ZbDgPUdcA0',
-//     {
-//         dialect: 'mysql',
-//         host: 'remotemysql.com'
-//         ,logging: false
-//     });
-
-// LOCAL
+Prod
 const sequelize = new Sequelize(
-    'datamoviz',
-    'root',
-    'EXua8ups1',
+    '5IU45193DS',
+    '5IU45193DS',
+    'ZbDgPUdcA0',
     {
         dialect: 'mysql',
-        host: 'localhost'
+        host: 'remotemysql.com'
         ,logging: false
-    });    
+    });
+
+// LOCAL
+// const sequelize = new Sequelize(
+//     'datamoviz',
+//     'root',
+//     'EXua8ups1',
+//     {
+//         dialect: 'mysql',
+//         host: 'localhost'
+//         ,logging: false
+//    });    
 
 
 module.exports = sequelize;
